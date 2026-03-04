82e8ba98b2537756f02445044f40fe9da280b0e3
skippp <pierre.peraldi31@gmail.com>
2026-03-04 23:27:17 +0100

    implement loader for greeting data in auth app route; refactor to use loader data instead of useQuery for improved performance

--- apps/web/src/routes/_auth/app/index.tsx
+++ apps/web/src/routes/_auth/app/index.tsx
@@ -1,28 +1,33 @@
-import { $getGreeting } from "@/lib/example-protected-server";
 import { createFileRoute } from "@tanstack/react-router";
-import { useQuery } from "@tanstack/react-query";
+
+import { $getGreeting } from "@/lib/example-protected-server";
 
 export const Route = createFileRoute("/_auth/app/")({
   component: AppIndex,
+  loader: async ({ context }) => {
+    const greeting = await context.queryClient.ensureQueryData({
+      queryKey: ["greeting"],
+      queryFn: () => $getGreeting(),
+    });
+
+    return {
+      greeting,
+    };
+  },
 });
 
 function AppIndex() {
   // we can also use the useAuth() or useAuthSuspense() hooks here from @repo/auth/tanstack/hooks
   const { user } = Route.useRouteContext();
-  const { data: greeting } = useQuery({
-    queryKey: ["greeting"],
-    queryFn: () => $getGreeting(),
-  });
+  const { greeting } = Route.useLoaderData();
 
   return (
     <div className="space-y-2 px-4 py-2">
       <h1 className="text-2xl font-bold">Welcome back, {user.name}!</h1>
 
-      <p className="text-sm text-muted-foreground">You are logged in as "{user.email}".</p>
-
       {greeting && (
         <p className="text-sm text-muted-foreground">
-          From protected server function: {greeting.message}
+          From protected server function : {greeting.message}
         </p>
       )}
     </div>
